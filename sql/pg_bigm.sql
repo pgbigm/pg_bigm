6c45a11ce8f10934694d31f2305967d140e9fb5e
MasaoFujii <masao.fujii@gmail.com>
2013-09-24 21:28:54 +0900

    Enable standard_conforming_strings before starting the tests.
    This is required to prevent the regression test with the backslash
    character from failing when standard_conforming_strings is disabled.
    
    Beena Emerson

--- sql/pg_bigm.sql
+++ sql/pg_bigm.sql
@@ -5,6 +5,8 @@ CREATE EXTENSION pg_bigm;
 SHOW pg_bigm.last_update;
 SET pg_bigm.last_update = '2013.09.18';
 
+SET standard_conforming_strings = on;
+
 SELECT likequery (NULL);
 SELECT likequery ('');
 SELECT likequery ('aBc023#*^&');
