pgbigm/pg_bigm

commit 0b09e86e2f35b2e64e290153c3def5f2ab72b940

Make pg_bigm available for parallel queries that was added in PostgreSQL9.6.
Reviewed by Masahiko Sawada.

pg_bigm--1.1--1.2.sql
@@ -17,3 +17,25 @@ BEGIN
     END IF;
 END;
 $$;
+
+/* Label whether the function is deemed safe for parallelism */
+DO $$
+DECLARE
+    pgversion TEXT;
+BEGIN
+    SELECT current_setting('server_version_num') INTO pgversion;
+    IF pgversion >= '90600' THEN
+        ALTER FUNCTION show_bigm(text) PARALLEL SAFE;
+        ALTER FUNCTION bigm_similarity(text, text) PARALLEL SAFE;
+        ALTER FUNCTION bigm_similarity_op(text, text) PARALLEL SAFE;
+        ALTER FUNCTION gin_extract_value_bigm(text, internal) PARALLEL SAFE;
+        ALTER FUNCTION gin_extract_query_bigm(text, internal, int2, internal, internal, internal, internal) PARALLEL SAFE;
+        ALTER FUNCTION gin_bigm_consistent(internal, int2, text, int4, internal, internal, internal, internal) PARALLEL SAFE;
+        ALTER FUNCTION gin_bigm_compare_partial(text, text, int2, internal) PARALLEL SAFE;
+        ALTER FUNCTION bigmtextcmp(text, text) PARALLEL SAFE;
+        ALTER FUNCTION likequery(text) PARALLEL SAFE;
+        ALTER FUNCTION pg_gin_pending_stats(index regclass) PARALLEL SAFE;
+        ALTER FUNCTION gin_bigm_triconsistent(internal, int2, text, int4, internal, internal, internal) PARALLEL SAFE;
+    END IF;
+END;
+$$;

pg_bigm--1.2.sql
@@ -90,3 +90,25 @@ BEGIN
     END IF;
 END;
 $$;
+
+/* Label whether the function is deemed safe for parallelism */
+DO $$
+DECLARE
+    pgversion TEXT;
+BEGIN
+    SELECT current_setting('server_version_num') INTO pgversion;
+    IF pgversion >= '90600' THEN
+        ALTER FUNCTION show_bigm(text) PARALLEL SAFE;
+        ALTER FUNCTION bigm_similarity(text, text) PARALLEL SAFE;
+        ALTER FUNCTION bigm_similarity_op(text, text) PARALLEL SAFE;
+        ALTER FUNCTION gin_extract_value_bigm(text, internal) PARALLEL SAFE;
+        ALTER FUNCTION gin_extract_query_bigm(text, internal, int2, internal, internal, internal, internal) PARALLEL SAFE;
+        ALTER FUNCTION gin_bigm_consistent(internal, int2, text, int4, internal, internal, internal, internal) PARALLEL SAFE;
+        ALTER FUNCTION gin_bigm_compare_partial(text, text, int2, internal) PARALLEL SAFE;
+        ALTER FUNCTION bigmtextcmp(text, text) PARALLEL SAFE;
+        ALTER FUNCTION likequery(text) PARALLEL SAFE;
+        ALTER FUNCTION pg_gin_pending_stats(index regclass) PARALLEL SAFE;
+        ALTER FUNCTION gin_bigm_triconsistent(internal, int2, text, int4, internal, internal, internal) PARALLEL SAFE;
+    END IF;
+END;
+$$;

html/release-1-2.html
@@ -28,6 +28,7 @@ ALTER EXTENSION pg_bigm UPDATE TO '1.2';
 
 <h2 id="release_note_2015-XX-YY">バージョン1.2 (2015-MM-DD リリース)</h2>
 <ul>
+<li><p>PostgreSQL9.6で導入されたパラレルクエリにpg_bigmを対応させました。(Fujii Masao)</p></li>
 <li>
 <p>性能向上のために<a href="http://www.postgresql.jp/document/current/html/gin-extensibility.html">triConsistent</a>メソッドをサポートしました。 (Fujii Masao)</p>
 <p>triConsistentメソッドにより、特に検索文字列の文字数が多いときの全文検索が高速になります。</p>

html/release-1-2_en.html
@@ -28,6 +28,7 @@ ALTER EXTENSION pg_bigm UPDATE TO '1.2';
 
 <h2 id="release_note_2015-XX-YY">Version 1.2 (released on 2015-XX-YY)</h2>
 <ul>
+<li><p>Make pg_bigm available for parallel queries that was introduced in PostgreSQL 9.6 (Fujii Masao)</p></li>
 <li>
 <p>Support <a href="http://www.postgresql.org/docs/current/static/gin-extensibility.html">triConsistent</a> method for better performance (Fujii Masao)</p>
 <p>The triConsistent method makes the full-text search using pg_bigm much faster especially when a long search phrase is used</p>

commit 4ab9ece2aca3c6cc12267e69c84b49115b3361eb

Fix pg_bigm so that it can be registered properly in PostgreSQL 9.5 or before.
Commit 0b09e86 introduced the bug which caused "CREATE EXTENSION pg_bigm" to
fail in PostgreSQL 9.5 or before. The cause of this failure is that
pg_bigm--1.2.sql included the DO command which tried to mark the functions
with PARALLEL SAFE label which those versions cannot handle. To prevent
those versions from emitting syntax failure when they look at PARALLEL SAFE
label, this commit changes so that "ALTER FUNCTION ... PARALLEL SAFE" command
is executed via EXECUTE command.

pg_bigm--1.1--1.2.sql
@@ -25,17 +25,17 @@ DECLARE
 BEGIN
     SELECT current_setting('server_version_num')::INTEGER INTO pgversion;
     IF pgversion >= 90600 THEN
-        ALTER FUNCTION show_bigm(text) PARALLEL SAFE;
-        ALTER FUNCTION bigm_similarity(text, text) PARALLEL SAFE;
-        ALTER FUNCTION bigm_similarity_op(text, text) PARALLEL SAFE;
-        ALTER FUNCTION gin_extract_value_bigm(text, internal) PARALLEL SAFE;
-        ALTER FUNCTION gin_extract_query_bigm(text, internal, int2, internal, internal, internal, internal) PARALLEL SAFE;
-        ALTER FUNCTION gin_bigm_consistent(internal, int2, text, int4, internal, internal, internal, internal) PARALLEL SAFE;
-        ALTER FUNCTION gin_bigm_compare_partial(text, text, int2, internal) PARALLEL SAFE;
-        ALTER FUNCTION bigmtextcmp(text, text) PARALLEL SAFE;
-        ALTER FUNCTION likequery(text) PARALLEL SAFE;
-        ALTER FUNCTION pg_gin_pending_stats(index regclass) PARALLEL SAFE;
-        ALTER FUNCTION gin_bigm_triconsistent(internal, int2, text, int4, internal, internal, internal) PARALLEL SAFE;
+        EXECUTE 'ALTER FUNCTION show_bigm(text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION bigm_similarity(text, text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION bigm_similarity_op(text, text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_extract_value_bigm(text, internal) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_extract_query_bigm(text, internal, int2, internal, internal, internal, internal) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_bigm_consistent(internal, int2, text, int4, internal, internal, internal, internal) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_bigm_compare_partial(text, text, int2, internal) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION bigmtextcmp(text, text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION likequery(text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION pg_gin_pending_stats(index regclass) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_bigm_triconsistent(internal, int2, text, int4, internal, internal, internal) PARALLEL SAFE';
     END IF;
 END;
 $$;

pg_bigm--1.2.sql
@@ -98,17 +98,17 @@ DECLARE
 BEGIN
     SELECT current_setting('server_version_num')::INTEGER INTO pgversion;
     IF pgversion >= 90600 THEN
-        ALTER FUNCTION show_bigm(text) PARALLEL SAFE;
-        ALTER FUNCTION bigm_similarity(text, text) PARALLEL SAFE;
-        ALTER FUNCTION bigm_similarity_op(text, text) PARALLEL SAFE;
-        ALTER FUNCTION gin_extract_value_bigm(text, internal) PARALLEL SAFE;
-        ALTER FUNCTION gin_extract_query_bigm(text, internal, int2, internal, internal, internal, internal) PARALLEL SAFE;
-        ALTER FUNCTION gin_bigm_consistent(internal, int2, text, int4, internal, internal, internal, internal) PARALLEL SAFE;
-        ALTER FUNCTION gin_bigm_compare_partial(text, text, int2, internal) PARALLEL SAFE;
-        ALTER FUNCTION bigmtextcmp(text, text) PARALLEL SAFE;
-        ALTER FUNCTION likequery(text) PARALLEL SAFE;
-        ALTER FUNCTION pg_gin_pending_stats(index regclass) PARALLEL SAFE;
-        ALTER FUNCTION gin_bigm_triconsistent(internal, int2, text, int4, internal, internal, internal) PARALLEL SAFE;
+        EXECUTE 'ALTER FUNCTION show_bigm(text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION bigm_similarity(text, text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION bigm_similarity_op(text, text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_extract_value_bigm(text, internal) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_extract_query_bigm(text, internal, int2, internal, internal, internal, internal) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_bigm_consistent(internal, int2, text, int4, internal, internal, internal, internal) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_bigm_compare_partial(text, text, int2, internal) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION bigmtextcmp(text, text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION likequery(text) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION pg_gin_pending_stats(index regclass) PARALLEL SAFE';
+        EXECUTE 'ALTER FUNCTION gin_bigm_triconsistent(internal, int2, text, int4, internal, internal, internal) PARALLEL SAFE';
     END IF;
 END;
 $$;