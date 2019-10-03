f0d502f510f55f7d52f90e5e8358731c4d398054
MasaoFujii <masao.fujii@gmail.com>
2019-10-03 22:05:41 +0900

    Update BIGM_LAST_UPDATE to '2019.10.03'.
    This commit also updates the documents, for upcoming release.

--- bigm_op.c
+++ bigm_op.c
@@ -25,7 +25,7 @@
 PG_MODULE_MAGIC;
 
 /* Last update date of pg_bigm */
-#define BIGM_LAST_UPDATE	"2016.10.11"
+#define BIGM_LAST_UPDATE	"2019.10.03"
 
 /* GUC variable */
 bool		bigm_enable_recheck = false;
